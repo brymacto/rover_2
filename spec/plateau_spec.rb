ad54a1809b8f15f75e1e1e8d718af2e7c731b95b
Bryan MacDiarmid <brymac@gmail.com>
2015-12-19 13:13:36 -0500

    Adds plateau specs

---
+++ spec/plateau_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../plateau'
+
+describe Plateau do 
+  subject { Plateau.new }
+
+  describe '#space_available?' do
+    xit 'returns false when space not available'
+    xit 'returns true when space is available'
+  end
+  
+end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -3,18 +3,27 @@ require_relative '../rover'
 describe Rover do 
   subject { Rover.new }
 
-  it 'turns right when prompted' do
-    subject.turn('r')
+  describe '#turn' do
+    it 'turns right when prompted' do
+      subject.turn('r')
 
-    expect(subject.direction).to eql('r')
+      expect(subject.direction).to eql('r')
+    end
+
+    it 'turns left when prompted' do
+      subject.turn('l')
+
+      expect(subject.direction).to eql('l')
+    end
   end
 
-  it 'turns left when prompted' do
-    subject.turn('l')
+  describe '#move' do
+    context 'path is not blocked by other rover' do
+      xit 'moves when prompted'
+    end
 
-    expect(subject.direction).to eql('l')
+    context 'path is blocked by other rover' do
+      xit 'does not move when prompted'
+    end
   end
-
-
-  
 end
