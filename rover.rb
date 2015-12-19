eda4cb9ab47b2958b5611c30d1bae06740f1dbb6
Bryan MacDiarmid <brymac@gmail.com>
2015-12-19 14:02:54 -0500

    Updates turn method

--- rover.rb
+++ rover.rb
@@ -1,2 +1,31 @@
 class Rover
+  DIRECTIONS = ['n', 'e', 's', 'w']
+  attr_accessor :direction
+
+  def initialize(args)
+    @x = args.fetch('x', 1)
+    @y = args.fetch('y', 1)
+    @direction = args.fetch('direction', 'e')
+    @plateau = args.fetch('plateau', Plateau.new)
+  end
+
+  def turn(direction)
+    raise "Direction must be 'l' or 'r'" if direction != 'l' && direction != 'r'
+    direction_index_change = 1 if direction == 'r'
+    direction_index_change = -1 if direction == 'l'
+    @direction = new_direction(direction_index_change)
+  end
+
+  private
+
+  def new_direction(direction_index_change)
+    new_direction_index = direction_index(@direction) + direction_index_change
+    new_direction_index = 0 if new_direction_index > 3 
+    new_direction_index = 3 if new_direction_index < 0 
+    DIRECTIONS[new_direction_index]
+  end
+
+  def direction_index(direction)
+    current_index = DIRECTIONS.index(@direction)
+  end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,19 +1,45 @@
 require_relative '../rover'
 
 describe Rover do 
-  subject { Rover.new(1, 1, 'e', double()) }
+  subject { Rover.new({x: 1, y: 1, direction: 'e', plateau: double()}) }
 
   describe '#turn' do
-    xit 'turns right when prompted' do
+    it 'turns right when prompted' do
       subject.turn('r')
 
-      expect(subject.direction).to eql('r')
+      expect(subject.direction).to eql('s')
     end
 
-    xit 'turns left when prompted' do
+    context 'current direction (w) is at end of DIRECTIONS constant' do
+      it 'turns right when prompted' do
+        subject.direction = 'w'
+
+        subject.turn('r')
+
+        expect(subject.direction).to eql('n')
+      end
+    end
+
+
+    it 'turns left when prompted' do
+      subject.direction = 's'
+
       subject.turn('l')
 
-      expect(subject.direction).to eql('l')
+      expect(subject.direction).to eql('e')
+    end
+    context 'current direction (n) is at beginning of DIRECTIONS constant' do
+      it 'turns left when prompted' do
+        subject.direction = 'n'
+
+        subject.turn('l')
+
+        expect(subject.direction).to eql('w')
+      end
+    end
+
+    it 'raises an error when direction is neither l nor r' do
+      expect { subject.turn('foo') }.to raise_error("Direction must be 'l' or 'r'")
     end
   end
 
