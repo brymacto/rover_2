acb0a3054a642da1a09b340c426822203ea9edb0
Bryan MacDiarmid <brymac@gmail.com>
2015-12-19 13:39:38 -0500

    Updates plateau specs

--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -4,7 +4,8 @@ describe Plateau do
   subject { Plateau.new }
 
   describe '#space_available?' do
-    xit 'returns false when space not available'
+    xit 'returns false when space occupied by other rover'
+    xit 'returns false when space is off the grid'
     xit 'returns true when space is available'
   end
   
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,29 +1,58 @@
 require_relative '../rover'
 
 describe Rover do 
-  subject { Rover.new }
+  subject { Rover.new(1, 1, 'e', double()) }
 
   describe '#turn' do
-    it 'turns right when prompted' do
+    xit 'turns right when prompted' do
       subject.turn('r')
 
       expect(subject.direction).to eql('r')
     end
 
-    it 'turns left when prompted' do
+    xit 'turns left when prompted' do
       subject.turn('l')
 
       expect(subject.direction).to eql('l')
     end
   end
 
+  describe '#status' do
+    xit 'returns the coords' do
+      result = subject.coords
+
+      expect(result).to eql({x: 1, y: 1, direction: 'e'})
+    end
+  end
+
   describe '#move' do
-    context 'path is not blocked by other rover' do
-      xit 'moves when prompted'
+    xit 'checks for availability before moving' do
+      space_x = 1
+      space_y = 2
+
+      subject.move
+
+      expect(subject.plateau).to receive(:space_available?).with(space_x, space_y)
     end
 
-    context 'path is blocked by other rover' do
-      xit 'does not move when prompted'
+    context 'space is available' do
+      xit 'moves when prompted' do
+        allow(subject.plateau).to receive(:subject_available?) { true }
+
+        subject.move
+
+        expect{ subject.move }.to change{ subject.status[x].from(1).to(2) }
+      end
+    end
+
+    context 'space is not available' do
+      xit 'does not move when prompted' do
+        allow(subject.plateau).to receive(:subject_available?) { false }
+
+        subject.move
+
+        expect{ subject.move }.to not_change{ subject.status }
+      end
     end
   end
 end
