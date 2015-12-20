481cd9bf8f9d30710338799a7ce96deaa6a62a87
Bryan MacDiarmid <brymac@gmail.com>
2015-12-20 11:02:00 -0500

    Moves class now in its own file

--- rover.rb
+++ rover.rb
@@ -1,4 +1,4 @@
-require 'pry'
+require './moves'
 
 class Rover
   DIRECTIONS = ['n', 'e', 's', 'w']
@@ -53,29 +53,5 @@ class Rover
 
   def direction_index(direction)
     DIRECTIONS.index(@direction)
-  end
-end
-
-class Moves
-  MOVES_BY_DIRECTION = { 
-    n: { axis: :y, distance: 1 }, 
-    e: { axis: :x, distance: 1 }, 
-    s: { axis: :y, distance: -1 }, 
-    w: { axis: :x, distance: -1 }
-  }
-
-  def axis(direction)
-    return MOVES_BY_DIRECTION[direction.to_sym][:axis]
-  end
-
-  def distance(direction, axis)
-    return 0 if !direction_and_axis_compatible?(direction, axis)
-    return MOVES_BY_DIRECTION[direction.to_sym][:distance]
-  end
-
-  def direction_and_axis_compatible?(direction, axis)
-    return true if [:e, :w].include?(direction.to_sym) && axis == :x
-    return true if [:n, :s].include?(direction.to_sym) && axis == :y
-    return false
   end
 end
