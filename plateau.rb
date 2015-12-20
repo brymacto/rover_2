8cb7e70eaf059cea31815ba5aba2304ae1a69e4e
Bryan MacDiarmid <brymac@gmail.com>
2015-12-20 10:49:39 -0500

    Refactors Plateau - method extraction

--- plateau.rb
+++ plateau.rb
@@ -1,20 +1,36 @@
 class Plateau
   def initialize(x_size, y_size, *rovers)
+    @max_x = x_size.to_i - 1
+    @max_y = y_size.to_i - 1
     @rovers = []
+
     rovers.each do |rover|
       @rovers << rover  
     end
-    @max_x = x_size.to_i - 1
-    @max_y = y_size.to_i - 1
   end
 
   def space_available?(space)
+    space_unoccupied?(space) && space_on_grid?(space)
+  end
+
+  private
+
+  def space_unoccupied?(space)
     result = true
+
     @rovers.each do |rover|
       result = false if rover.position == space
     end
+
+    result
+  end
+
+  def space_on_grid?(space)
+    result = true
+    
     result = false if space[:x] < 0 || space[:y] < 0
     result = false if space[:x] > @max_x || space[:y] > @max_y
+
     result
   end
 end
