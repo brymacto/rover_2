9a955366f02dca95bed4cb6317ccdf377319f9e7
Bryan MacDiarmid <brymac@gmail.com>
2015-12-20 10:56:50 -0500

    Refactors Plateau - instance variables

--- plateau.rb
+++ plateau.rb
@@ -3,6 +3,7 @@ class Plateau
     @max_x = x_size.to_i - 1
     @max_y = y_size.to_i - 1
     @rovers = []
+    @space_being_checked = {}
 
     rovers.each do |rover|
       @rovers << rover  
@@ -10,20 +11,21 @@ class Plateau
   end
 
   def space_available?(space)
-    space_unoccupied?(space) && space_on_grid?(space)
+    @space_being_checked = space
+    space_unoccupied? && space_on_grid?
   end
 
   private
 
-  def space_unoccupied?(space)
-    @rovers.none? { |rover| rover.position == space }
+  def space_unoccupied?
+    @rovers.none? { |rover| rover.position == @space_being_checked }
   end
 
-  def space_on_grid?(space)
+  def space_on_grid?
     result = true
     
-    result = false if space[:x] < 0 || space[:y] < 0
-    result = false if space[:x] > @max_x || space[:y] > @max_y
+    result = false if @space_being_checked[:x] < 0 || @space_being_checked[:y] < 0
+    result = false if @space_being_checked[:x] > @max_x || @space_being_checked[:y] > @max_y
 
     result
   end
