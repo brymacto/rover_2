7f6d599919d77c735517853a1d90eac11d2af870
Bryan MacDiarmid <brymac@gmail.com>
2015-12-20 10:37:14 -0500

    Plateau checks for space off y and x axes

--- plateau.rb
+++ plateau.rb
@@ -1,9 +1,11 @@
 class Plateau
-  def initialize(*rovers)
+  def initialize(x_size, y_size, *rovers)
     @rovers = []
     rovers.each do |rover|
       @rovers << rover  
     end
+    @max_x = x_size.to_i - 1
+    @max_y = y_size.to_i - 1
   end
 
   def space_available?(space)
@@ -11,6 +13,8 @@ class Plateau
     @rovers.each do |rover|
       result = false if rover.position == space
     end
+    result = false if space[:x] < 0 || space[:y] < 0
+    result = false if space[:x] > @max_x || space[:y] > @max_y
     result
   end
 end
--- spec/plateau_spec.rb
+++ spec/plateau_spec.rb
@@ -3,7 +3,7 @@ require_relative '../plateau'
 describe Plateau do 
   let(:rover_1) { double }
   let(:rover_2) { double }
-  subject { Plateau.new(rover_1, rover_2) }
+  subject { Plateau.new(30, 30, rover_1, rover_2) }
   
 
   describe '#space_available?' do
@@ -11,13 +11,47 @@ describe Plateau do
       space = { x: 2, y: 1 }
       allow(rover_1).to receive(:position) { { x: 1, y: 1 } }
       allow(rover_2).to receive(:position) { { x: 2, y: 1 } }
+
       expect(rover_1).to receive(:position) 
       expect(rover_2).to receive(:position) 
+
       result = subject.space_available?(space)
+
       expect(result).to eql(false)
     end
-    xit 'returns false when space is off the grid'
-    xit 'returns true when space is available'
+
+    it 'returns false when space is off the y axis' do
+      space = { x: 1, y: -1 }
+      allow(rover_1).to receive(:position) { { x: 1, y: 1 } }
+      allow(rover_2).to receive(:position) { { x: 2, y: 1 } }
+
+      result = subject.space_available?(space)
+
+      expect(result).to eql(false)
+    end
+
+    it 'returns false when space is off the x axis' do
+      space = { x: 30, y: 1 }
+      allow(rover_1).to receive(:position) { { x: 1, y: 1 } }
+      allow(rover_2).to receive(:position) { { x: 2, y: 1 } }
+
+      result = subject.space_available?(space)
+
+      expect(result).to eql(false)
+    end
+    
+    it 'returns true when space is available' do
+      space = { x: 29, y: 29 }
+      allow(rover_1).to receive(:position) { { x: 1, y: 1 } }
+      allow(rover_2).to receive(:position) { { x: 2, y: 1 } }
+
+      expect(rover_1).to receive(:position) 
+      expect(rover_2).to receive(:position) 
+
+      result = subject.space_available?(space)
+
+      expect(result).to eql(true)
+    end
   end
   
 end
